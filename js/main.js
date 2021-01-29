16c4acd0c975b59640860a7320191779cc54935a
iii <blbaileyiii.naa@gmail.com>
2021-01-29 12:09:37 -0800

    adding notes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const links = [
     {label: "Notes", url: "week01/index.html"},
     {label: "Notes", url: "week02/index.html"},
-    {label: "Notes", url: "week03/index.html"}
+    {label: "Notes", url: "week03/index.html"},
+    {label: "Notes", url: "week04/index.html"},
+    {label: "Notes", url: "week05/index.html"},
+    {label: "Notes", url: "week06/index.html"},
+    {label: "Notes", url: "week07/index.html"},
+    {label: "Notes", url: "week08/index.html"},
+    {label: "Notes", url: "week09/index.html"},
+    {label: "Notes", url: "week10/index.html"},
+    {label: "Notes", url: "week11/index.html"},
+    {label: "Notes", url: "week12/index.html"},
+    {label: "Notes", url: "week13/index.html"},
+    {label: "Notes", url: "week14/index.html"}
 ];
 
 let olLinks = document.querySelector('.links');
