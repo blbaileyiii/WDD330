ff8b49274f358731a7937fa8393270c779529089
iii <blbaileyiii.naa@gmail.com>
2021-02-10 09:41:40 -0800

    a

--- js/challengeOne.js
+++ js/challengeOne.js
@@ -167,7 +167,3 @@ function countRemaining(){
     let tasksRemaining = document.querySelector('.tasks-remaining')
     tasksRemaining.textContent = filterIncomplete().length + ' tasks left';
 }
-
-export {
-    addTask
-}
