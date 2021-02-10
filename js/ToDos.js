cd310befb948cbc4cbec8ad5234e2cc5553f93ed
iii <blbaileyiii.naa@gmail.com>
2021-02-10 15:21:37 -0800

    a

--- js/ToDos.js
+++ js/ToDos.js
@@ -17,7 +17,7 @@ export default class ToDos {
     addTodo(src) {
         console.log(this.toDoList);
         this.toDoList[this.toDoList.length] = {id: Date.now(), content: src.value, completed: false};
-        
+        console.log(this.toDoList);
     }
 
     listTodos() {
