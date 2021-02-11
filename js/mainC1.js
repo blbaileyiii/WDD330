a3d47343214ed4a414a04c6c031314c44e5841fc
iii <blbaileyiii.naa@gmail.com>
2021-02-10 16:06:48 -0800

    a

--- js/mainC1.js
+++ js/mainC1.js
@@ -2,7 +2,7 @@ import ToDos from './ToDos.js';
 
 let todos = new ToDos();
 
-document.querySelector('.add-task').addEventListener("click", todos.addTodo.bind(todos, this.value));
+document.querySelector('.add-task').addEventListener("click", todos.addTodo.bind(todos, this.target.value));
 
 function renderToDoList(list, element) {
 
