f1605c55cdeb036c659dce49173ac51a63830255
iii <blbaileyiii.naa@gmail.com>
2021-02-19 09:24:10 -0800

    a

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const links = [
-    {label: "Notes", url: "week01/index.html"},
-    {label: "Notes", url: "week02/index.html"},
-    {label: "Notes", url: "week03/index.html"},
-    {label: "Notes", url: "week04/index.html"},
-    {label: "Notes", url: "week05/index.html"},
-    {label: "Notes", url: "week06/index.html"},
-    {label: "Notes", url: "week07/index.html"},
-    //{label: "Notes", url: "week08/index.html"},
-    //{label: "Notes", url: "week09/index.html"},
-    //{label: "Notes", url: "week10/index.html"},
-    //{label: "Notes", url: "week11/index.html"},
-    //{label: "Notes", url: "week12/index.html"},
-    //{label: "Notes", url: "week13/index.html"},
-    //{label: "Notes", url: "week14/index.html"}
+    {label: "Notes", url: "week01/"},
+    {label: "Notes", url: "week02/"},
+    {label: "Notes", url: "week03/"},
+    {label: "Notes", url: "week04/"},
+    {label: "Notes", url: "week05/"},
+    {label: "Notes", url: "week06/"},
+    {label: "Notes", url: "week07/"},
+    //{label: "Notes", url: "week08/"},
+    //{label: "Notes", url: "week09/"},
+    //{label: "Notes", url: "week10/"},
+    //{label: "Notes", url: "week11/"},
+    //{label: "Notes", url: "week12/"},
+    //{label: "Notes", url: "week13/"},
+    //{label: "Notes", url: "week14/"}
 ];
 
 let olLinks = document.querySelector('.links');
