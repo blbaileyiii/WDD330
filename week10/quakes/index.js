5f458760561cc4b86df3d169c85f88b104dca616
iii <blbaileyiii.naa@gmail.com>
2021-03-11 05:26:56 -0800

    a

--- week10/quakes/index.js
+++ week10/quakes/index.js
@@ -2,4 +2,14 @@ import { getJSON, getLocation } from './utilities.js';
 
 const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 
-console.log(getLocation());
+function getLocationURL() {
+    return new Promise(function(resolve, reject) {
+      getLocation.then(function (data) {
+        var url = baseUrl + "&latitude=" + data.coords.latitude + "&longitude=" + data.coords.longitude + "&maxradiuskm=100";
+        resolve(url);
+      });
+    });
+}
+
+console.log(getLocationURL());
+
